ef731dbbb0be9cafd54bc140329e8ab178aeed50
lakshaysaini1464 <lakshaysaini1464@gmail.com>
2025-08-04 23:00:05 +0530

    mongo

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,17 @@
-import mongoose from "mongoose";
+// db.js
+import mongoose from 'mongoose';
 
-const connectDB = async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('connected');
-    }catch(error){
-        console.log("error occur lakshay",error);
-    }
-}
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`MongoDB connection error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
 export default connectDB;
